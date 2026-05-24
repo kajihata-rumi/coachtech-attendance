65151afc5d0c788d7ac84b18d3d20598c3287fc2
kajihata-rumi <owlish.jp@gmail.com>
2026-05-24 17:47:25 +0900

    Refactor admin staff list layout

--- src/resources/views/admin/staff/list.blade.php
+++ src/resources/views/admin/staff/list.blade.php
@@ -1,28 +1,8 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>スタッフ一覧</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <header class="header">
-        <a class="header__logo" href="/admin/attendance/list">
-            <img class="header__logo-img" src="{{ asset('images/coachtech-logo.png') }}" alt="COACHTECH">
-        </a>
+@extends('layouts.admin')
 
-        <nav>
-            <a href="/admin/attendance/list">勤怠一覧</a>
-            <a href="/admin/staff/list">スタッフ一覧</a>
-            <a href="{{ route('stamp_correction_request.list') }}">申請一覧</a>
+@section('title', 'スタッフ一覧')
 
-            <form action="{{ route('admin.logout') }}" method="POST" style="display:inline;">
-                @csrf
-                <button type="submit">ログアウト</button>
-            </form>
-        </nav>
-    </header>
-
+@section('content')
     <main class="admin-staff-list">
         <h1 class="admin-staff-list__title">スタッフ一覧</h1>
 
@@ -34,6 +14,7 @@
                     <th>月次勤怠</th>
                 </tr>
             </thead>
+
             <tbody>
                 @foreach ($staffs as $staff)
                     <tr>
@@ -47,5 +28,4 @@
             </tbody>
         </table>
     </main>
-</body>
-</html>
+@endsection
